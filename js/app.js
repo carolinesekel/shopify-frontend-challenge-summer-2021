f3173abc54771e289785f3c669958b77fcef13d5
Caroline Sekel <carolinesekel@carolines-mbp-2.lan>
2021-01-13 16:32:21 -0500

    accessing data from OMDb API and displaying movie title and year based on sample search term. up next: using input from search bar and running function with real input value

--- js/app.js
+++ js/app.js
@@ -1,16 +1,32 @@
 const apiKey = config.MY_KEY;
-let url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=Ram";
+let url = "http://www.omdbapi.com/?apikey=" + apiKey + "&s=";
 
 const resultsLabel = document.getElementById("results-label");
+const resultsList = document.getElementById("results-list");
+const nominationsList = document.getElementById("nominations-list");
 
-fetch(url)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        // Work with JSON data here
-        console.log(data)
-    })
-    .catch((err) => {
-        // Do something for an error here
-    })
+
+function newSearch(searchTerm) {
+    updatedHtml = ' "' + searchTerm + '"';
+    resultsLabel.insertAdjacentHTML("beforeend", updatedHtml)
+    const finalUrl = url + searchTerm;
+    fetch(finalUrl)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            // Work with JSON data here
+            console.log(data)
+            data.Search.forEach((movie) => {
+                console.log(movie.Title)
+                console.log(movie.Year)
+                var resultItem = document.createElement('li');
+                resultItem.textContent = `${movie.Title} (${movie.Year})`;
+                resultsList.appendChild(resultItem);
+            })
+        })
+        .catch((err) => {
+            // Do something for an error here
+        })
+}
+newSearch("Ram");
